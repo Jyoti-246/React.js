79008c1b5ca963276f8a9f4188d46580ef233f6a
Jyoti-246 <jyotikedia203@gmail.com>
2026-07-13 08:42:35 +0530

    Delete usingMUI directory

--- usingMUI/src/Pages/course.js
+++
@@ -1,7 +0,0 @@
-export const courses = [
-    {id:'MERN001', course_name:'MERN_Stack', price:10000, duration:'5 months'},
-    {id:'JAVA002', course_name:'Java_Full_Stack', price:15000, duration:'7 months'},
-    {id:'PYTHON003', course_name:'Python_Full_Stack', price:11000, duration:'1 months'},
-    {id:'REACT004', course_name:'React.JS', price:20000, duration:'9 months'},
-    {id:'C005', course_name:'C++', price:19000, duration:'5 months'},
-]
