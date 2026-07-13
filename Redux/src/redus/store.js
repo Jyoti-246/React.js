531ff37c72fc0826a056241753d399cda51a3182
Jyoti-246 <jyotikedia203@gmail.com>
2026-07-13 08:41:06 +0530

    Delete Redux directory

--- Redux/src/redus/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counterSlice/counterSlice'
-const store = configureStore({
-  reducer: {
-    counter : counterReducer
-  },
-})
-
-export default store;
